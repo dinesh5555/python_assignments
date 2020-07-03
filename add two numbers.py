33fbcbe1de8f92bd6ffe07fa66640ce1ab84a756
dinesh5555 <67151298+dinesh5555@users.noreply.github.com>
2020-07-03 14:42:49 +0530

    Add files via upload

---
+++ add two numbers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+a=int(input("enter the value of a"))
+b=float(input("enter the value of b"))
+sum=int(a)+float(b)
+print("sum of two numbers is",sum)
+
